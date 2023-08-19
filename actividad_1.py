baebde4f163b0add770fa19f3fa404a9642a51d7
Kosmonautt <b.contreras.2@ug.uchile.cl>
2023-08-18 20:06:15 -0400

    The python file prints the request made by the broswer.

---
+++ actividad_1.py
@@ -0,0 +1,29 @@
+import socket
+
+# tamaño del buffer del server
+buff_size = 1024
+
+# dirección del socket server
+server_adress = ('localhost', 8000)
+
+# se crea el server
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# se le hace bind a la dirección especificada 
+server_socket.bind(server_adress)
+
+# el server puede escuchar solo una request a la vez
+server_socket.listen(1)
+
+# se espera una request, cuando se recibe se 
+# crea un nuevo socket 
+new_socket, new_adress = server_socket.accept()
+
+# recibimos el mensaje (en bytes)
+message = new_socket.recv(buff_size)
+
+print(message.decode())
+
+# se cierra la conexión con el socket
+new_socket.close()
+
